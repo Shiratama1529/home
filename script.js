550ad6a3494212c5ec78caec491d1fa15f2d085e
Shiratama1529 <65014220+Shiratama1529@users.noreply.github.com>
2020-08-01 21:33:52 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#menu-icon').click(function() {
+        $('#menu-modal-wrapper').fadeIn();
+    });
+
+    $('#menu-modal-close').click(function() {
+        $('#menu-modal-wrapper').fadeOut();
+    });
+});
